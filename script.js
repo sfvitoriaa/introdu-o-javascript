1050beb3419806204f069971b1d98d422d127791
Sofia <0001046198@senaimgaluno.com.br>
2025-12-11 18:46:04 -0300

    m

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
-const titulodahome = document.getElementById("title-home")
+const tituloDaHome = document.getElementById("title-home")
 
-titulodahome.innerHTML = "Sofia"
-titulodahome.style.color = "#c2c2c2"
-titulodahome.setAttribute("style", "color: #101010; font-size: 80px")
+tituloDaHome.innerHTML = "Sofia"
+tituloDaHome.style.color = "#c2c2c2"
+tituloDaHome.setAttribute("style", "color: #101010; font-size: 80px")
 
-const buttonHome = document.getElementById("bnt-home")
+const idHome = document.getElementById("home")
+// console.log('innerText', idHome.innerText)
+// console.log('innerHTML', idHome.innerHTML)
 
-buttonHome.innerHTML = "Clique aqui!"
-buttonHome.addEventListener('click', () => {titulodahome.innerHTML = "Seu futuro começa hoje!!!"; document.querySelector(".bg2").setAttribute("style", "background-color: red;");});
+const buttonHome = document.getElementById("btn-home")
 
+// const classBg2 = document.querySelector(".bg2")
+const classBg2 = document.getElementsByClassName("bg2")
+// console.log('elemento', classBg2)
 
+buttonHome.innerHTML = "Clique aqui"
+
+const temAClasseCard = document.querySelectorAll('.card')
+console.log('classe card', temAClasseCard)
+
+buttonHome.addEventListener('click', () => {
+    tituloDaHome.innerHTML = "Sofia"
+    temAClasseCard[2].setAttribute('style', 'background-color: red')
+    // classBg2.classList.toggle('red')
+    // for (let index = 0; index < temAClasseCard.length; index++) {
+    //     temAClasseCard[index].classList.toggle('red') 
+    // }
+
+   
+
+})
+
+// Ao clicar no botão 
+// O h1 da página deverá receber o seguinte texto: Seu Futuro começa hoje!!!
+const secondTitleId = document.getElementById("second-title")
+const secondTitleQuery = document.querySelector("#second-title")
+// console.log('bg2', classBg2)
+// console.log('secondTitleId', secondTitleId)
+
+// mude a cor do background-color de bg2 para red ao clicar no botão
